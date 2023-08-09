85502b047043e86d73bf3ae4be5fc824c9269a5d
Stephanie Panah <stephanie.panah@gmail.com>
2023-08-09 11:20:13 -0700

    ok

--- unittest.py
+++ unittest.py
@@ -1,4 +1,4 @@
 def test_flake8_linting(self):
-    result = !flake8 "C:/DEV/JenkinsTest/jenkins"
+    result = !flake8 "C:/DEV/pipelines/NewTest"
     self.assertEqual(result, "", "There are linting errors")
     
